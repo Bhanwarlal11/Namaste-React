ad65f4ecf1b938c74d57c2e498b75e68eafbb543
bhanwarlal11 <bhawarpatel2016@gmail.com>
2024-02-15 18:58:44 +0530

    custom Hooks

--- Ep-09 Optimising Our App/src/components/RestaurantMenu.js
+++ Ep-09 Optimising Our App/src/components/RestaurantMenu.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import { useEffect,useState } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
-import { MENU_API } from "../utils/constants";
+import useRestuarantMenu from '../utils/useRestuarantMenu';
 
 const RestaurantMenu = () => {
   
-  const [resInfo,setResInfo] = useState(null);
-    const { resId } = useParams();
+  const { resId } = useParams();
+  const resInfo = useRestuarantMenu(resId);
 
-useEffect(() =>{
-  fetchMenu();
-},[]);
-
-  
-
-const fetchMenu = async ()=>{
-    const data = await fetch(MENU_API + resId)
-    const json = await data.json();
-    console.log(json)
-    setResInfo(json.data);
-}
 
 if(resInfo===null) return <Shimmer/>;
 
