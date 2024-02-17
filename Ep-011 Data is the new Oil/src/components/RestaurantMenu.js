c357d5bafbda4c94d70ec9949909dff5c6150cc7
bhanwarlal11 <bhawarpatel2016@gmail.com>
2024-02-17 20:53:18 +0530

    ep-11 data is the new world

--- Ep-011 Data is the new Oil/src/components/RestaurantMenu.js
+++ Ep-011 Data is the new Oil/src/components/RestaurantMenu.js
@@ -1,38 +1,44 @@
-import React from 'react';
+import React from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
-import useRestuarantMenu from '../utils/useRestuarantMenu';
-
+import useRestuarantMenu from "../utils/useRestuarantMenu";
+import RestaurantCategory from "./RestaurantCategory.js"
 const RestaurantMenu = () => {
-  
   const { resId } = useParams();
   const resInfo = useRestuarantMenu(resId);
 
+  if (resInfo === null) return <Shimmer />;
 
-if(resInfo===null) return <Shimmer/>;
+  //destructuring & optional Chaining
+  //->menu path
+  const { name, cuisines, costForTwoMessage } =
+    resInfo?.cards[0]?.card?.card?.info;
+  //->menu list item path
+  const { itemCards } =
+    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
+  // console.log(resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards);
 
+  const categories =
+    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+      (c) =>
+        c.card?.card?.["@type"] ==
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
+// console.log(categories);
 
-//destructuring & optional Chaining
-//->menu path
-const { name, cuisines, costForTwoMessage } = resInfo?.cards[0]?.card?.card?.info;
-//->menu list item path
-const { itemCards } = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
-console.log(itemCards)
+  return (
+    <div className="text-center">
+      <h1 className="font-bold my-6 text-2xl">{name}</h1>
+      <p className="font-bold text-lg">
+        {cuisines.join(", ")} - {costForTwoMessage}
+      </p>
 
-
-  return(
-    <div>
-      
-        <h1>{name}</h1>
-        <p>{cuisines.join(', ')} - {costForTwoMessage}</p>
-        <h2>Menu</h2>
-        {itemCards.map((item)=>(
-          <li key={item.card.info.id}>
-            {item.card.info.name} -{" Rs."}
-            {item.card.info.price /100 || item.card.info.defaultPrice /100} 
-          </li>
+      {/* category accordian */}
+      {
+        categories.map((category)=>(
+          <RestaurantCategory data={category?.card?.card}/>
         ))
-        }
+      }
     </div>
   );
 };
