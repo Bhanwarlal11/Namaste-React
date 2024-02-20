d8532e805f59b6abb54dceb7f5e6cd406de2f9ed
bhanwarlal11 <bhawarpatel2016@gmail.com>
2024-02-20 22:29:43 +0530

    20feb first commit

--- Ep-011 Data is the new Oil/src/components/RestaurantCategory.js
+++ Ep-011 Data is the new Oil/src/components/RestaurantCategory.js
@@ -1,19 +1,32 @@
+import { useState } from "react";
 import ItemList from "./ItemList";
 
-const RestaurantCategory = ({ data }) => {
-  // console.log(data)
+const RestaurantCategory = ({ data , showItems , setShowIndex}) => {
+
+  // //now i did not want to restraurant category has own power
+  // const [showItems,setShowItems] = useState(false);
+  console.log(data)
+  const handleClick = ()=>{
+    setShowIndex();
+  }
+
+
+
   return (
     <div>
       {/* Accordian Header */}
       <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4">
-        <div className="flex justify-between">
+        <div 
+          className="flex justify-between cursor-pointer"
+          onClick={handleClick}
+        >
           <span className="font-bold text-lg">
             {data.title} ({data.itemCards.length})
           </span>
           <span>⬇️</span>
         </div>
         {/* Accordian Boday */}
-        <ItemList items={data.itemCards} />
+        {showItems && <ItemList items={data.itemCards} />}
       </div>
     </div>
   );
