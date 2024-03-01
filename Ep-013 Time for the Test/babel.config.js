e9d3a60a015753cb2c1dabfac462afe3771be46f
bhanwarlal11 <bhawarpatel2016@gmail.com>
2024-03-01 20:30:48 +0530

    ep-13

--- Ep-013 Time for the Test/babel.config.js
+++ Ep-013 Time for the Test/babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   presets: [["@babel/preset-env", { targets: { node: "current" } }],
-          ["babel/preset-react", {runtime: "automatic"}]
-  ],
+          ["@babel/preset-react", {runtime: "automatic"}]
+],
 };
--- Ep-013 Time for the Test/src/components/__test__/contact.test.js
+++ Ep-013 Time for the Test/src/components/__test__/contact.test.js
@@ -1,12 +1,46 @@
 import { render,screen } from "@testing-library/react";
 import Contact from "../Contact";
-import "testing-library/jest-dom"
+import "@testing-library/jest-dom"
 
 
 test("should load contact us component" , ()=>{
     render(<Contact/>);
-
+    // Querying
     const heading = screen.getByRole("heading");
     //Assertion
     expect(heading).toBeInTheDocument();
 });
+
+test("should load button inside contact us component" , ()=>{
+    render(<Contact/>);
+    // Querying
+    const button = screen.getByRole("button");
+    // const button = screen.getByText("Submit");
+
+    //Assertion
+    expect(button).toBeInTheDocument();
+});
+
+test("should load input name inside contact us component" , ()=>{
+    render(<Contact/>);
+// Querying
+    const inputName = screen.getByPlaceholderText("Name");
+
+    //Assertion
+    expect(inputName).toBeInTheDocument();
+});
+
+test("should load 2 input boxes on the Contact component",()=>{
+    render(<Contact/>)
+
+    // Querying
+    const inputBoxes = screen.getAllByRole("textbox");
+
+    // console.log(inputBoxes.length);
+
+    //Assertion
+    expect(inputBoxes.length).toBe(2);
+})
+
+
+
